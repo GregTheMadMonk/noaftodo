c30dc01a7a10e65b8cbb91454d13e4d04b456422
GregTheMadMonk <yagreg7@gmail.com>
2020-09-14 01:33:13 +0300

    Starting migrate to cmake

--- tools/docgen.sh
+++ tools/docgen.sh
@@ -1,23 +1,34 @@
 #!/bin/sh
 
-printf "Command mode commands:\n%s" "$(grep -i 'command \"' src/noaftodo_def.cpp | sort | sed 's/.*\/\/ command \"/\t:/g;s/\".*- /\n\t\t/g')" > doc/cmd.doc.gen
-printf "Command-line arguments:\n%s" "$(grep -i 'argument \"' src/noaftodo.cpp | sort | sed 's/.*\/\/ argument \"/\t/g;s/\".*- /\n\t\t/g')" > doc/arg.doc.gen
-printf "%s" "$(grep -i 'LISTVIEW column \"' src/noaftodo_def.cpp | sort | sed 's/.*\/\/ LISTVIEW column \"/\t/g;s/\".*- /\n\t\t/g')" > doc/cols.lview.doc.gen
-printf "%s" "$(grep -i 'NORMAL column \"' src/noaftodo_def.cpp | sort | sed 's/.*\/\/ NORMAL column \"/\t/g;s/\".*- /\n\t\t/g')" > doc/cols.norm.doc.gen
-printf "%s" "$(grep -i 'status field \"' src/noaftodo_def.cpp | sort | sed 's/.*\/\/ status field \"/\t/g;s/\".*- /\n\t\t/g')" > doc/fields.status.doc.gen
+if [ "$1" == "" ]; then
+	echo "Directory unspecified"
+	exit 1
+fi
+DIR="$1"
+
+echo "Generating docs and embedded code for $DIR"
+
+mkdir -p gen/doc
+mkdir -p gen/src
+
+printf "Command mode commands:\n%s" "$(grep -i 'command \"' $DIR/src/noaftodo_def.cpp | sort | sed 's/.*\/\/ command \"/\t:/g;s/\".*- /\n\t\t/g')" > gen/doc/cmd.doc.gen
+printf "Command-line arguments:\n%s" "$(grep -i 'argument \"' $DIR/src/noaftodo.cpp | sort | sed 's/.*\/\/ argument \"/\t/g;s/\".*- /\n\t\t/g')" > gen/doc/arg.doc.gen
+printf "%s" "$(grep -i 'LISTVIEW column \"' $DIR/src/noaftodo_def.cpp | sort | sed 's/.*\/\/ LISTVIEW column \"/\t/g;s/\".*- /\n\t\t/g')" > gen/doc/cols.lview.doc.gen
+printf "%s" "$(grep -i 'NORMAL column \"' $DIR/src/noaftodo_def.cpp | sort | sed 's/.*\/\/ NORMAL column \"/\t/g;s/\".*- /\n\t\t/g')" > gen/doc/cols.norm.doc.gen
+printf "%s" "$(grep -i 'status field \"' $DIR/src/noaftodo_def.cpp | sort | sed 's/.*\/\/ status field \"/\t/g;s/\".*- /\n\t\t/g')" > gen/doc/fields.status.doc.gen
 
 printf "#include <string>
 std::string HELP = \"%s\";
 std::string CMDS_HELP = \"%s\";
 std::string TCONF = \"%s\";" \
-"$(sed 's/\\/\\\\/g' doc/arg.doc.gen | sed 's/\t/\\t/g' | sed ':a;N;$!ba;s/\n/\\n/g' | sed 's/\"/\\"/g')" \
-"$(sed 's/\*/\\\*/g' doc/cmd.doc.gen | sed 's/\\/\\\\/g' | sed 's/\t/\\t/g' | sed '0~2s/^/**/g;0~2s/$/**/g' | sed ':a;N;$!ba;s/\n/\\n/g' | sed 's/\"/\\"/g')" \
-"$(sed 's/\\/\\\\/g' noaftodo.conf.template | sed ':a;N;$!ba;s/\n/\\n/g' | sed 's/\"/\\"/g')" \
-> src/noaftodo_embed.cpp
+"$(sed 's/\\/\\\\/g' gen/doc/arg.doc.gen | sed 's/\t/\\t/g' | sed ':a;N;$!ba;s/\n/\\n/g' | sed 's/\"/\\"/g')" \
+"$(sed 's/\*/\\\*/g' gen/doc/cmd.doc.gen | sed 's/\\/\\\\/g' | sed 's/\t/\\t/g' | sed '0~2s/^/**/g;0~2s/$/**/g' | sed ':a;N;$!ba;s/\n/\\n/g' | sed 's/\"/\\"/g')" \
+"$(sed 's/\\/\\\\/g' $DIR/noaftodo.conf.template | sed ':a;N;$!ba;s/\n/\\n/g' | sed 's/\"/\\"/g')" \
+> gen/src/noaftodo_embed.cpp
 
-LIST_V=$(grep ' LIST_V = ' src/noaftodo.h | sed 's/.*LIST_V = //g;s/\;//g')
-CONF_V=$(grep ' CONF_V = ' src/noaftodo.h | sed 's/.*CONF_V = //g;s/\;//g')
-MINOR_V=$(grep ' MINOR_V = ' src/noaftodo.h | sed 's/.*MINOR_V = //g;s/\;//g')
+LIST_V=$(grep ' LIST_V = ' $DIR/src/noaftodo.h | sed 's/.*LIST_V = //g;s/\;//g')
+CONF_V=$(grep ' CONF_V = ' $DIR/src/noaftodo.h | sed 's/.*CONF_V = //g;s/\;//g')
+MINOR_V=$(grep ' MINOR_V = ' $DIR/src/noaftodo.h | sed 's/.*MINOR_V = //g;s/\;//g')
 
 printf ".TH NOAFTODO 1 \"July 2020\" \"%s.%s.%s\" \"NOAFtodo man page\"
 .SH NAME
@@ -57,11 +68,11 @@ Apology for bad english and probably not very useful manpage.
 "$LIST_V" \
 "$CONF_V" \
 "$MINOR_V" \
-"$(sed '1d;s/\t//g;s/-/\\-/g;0~2s/^/.HP\n/g;0~2s/$/\n.IP/g' doc/arg.doc.gen)" \
-"$(sed '1d;s/\t//g;s/-/\\-/g;0~2s/^/.HP\n.B\n/g;0~2s/$/\n.IP/g' doc/cmd.doc.gen)" \
-"$(sed 's/\t//g;s/-/\\-/g;1~2s/^/.HP\n.B\n/g;1~2s/$/\n.IP/g' doc/cols.norm.doc.gen)" \
-"$(sed 's/\t//g;s/-/\\-/g;1~2s/^/.HP\n.B\n/g;1~2s/$/\n.IP/g' doc/cols.lview.doc.gen)" \
-"$(sed 's/\t//g;s/-/\\-/g;1~2s/^/.HP\n.B\n/g;1~2s/$/\n.IP/g' doc/fields.status.doc.gen)" \
-"$(sed '1d;s/\t//g;2s/.*/\.SS CONFIG VERSION MISMATCH/g;4s/.*/\.SS LIST VERSION MISMATCH/g' doc/troubleshooting.doc)" > doc/noaftodo.man
+"$(sed '1d;s/\t//g;s/-/\\-/g;0~2s/^/.HP\n/g;0~2s/$/\n.IP/g' gen/doc/arg.doc.gen)" \
+"$(sed '1d;s/\t//g;s/-/\\-/g;0~2s/^/.HP\n.B\n/g;0~2s/$/\n.IP/g' gen/doc/cmd.doc.gen)" \
+"$(sed 's/\t//g;s/-/\\-/g;1~2s/^/.HP\n.B\n/g;1~2s/$/\n.IP/g' gen/doc/cols.norm.doc.gen)" \
+"$(sed 's/\t//g;s/-/\\-/g;1~2s/^/.HP\n.B\n/g;1~2s/$/\n.IP/g' gen/doc/cols.lview.doc.gen)" \
+"$(sed 's/\t//g;s/-/\\-/g;1~2s/^/.HP\n.B\n/g;1~2s/$/\n.IP/g' gen/doc/fields.status.doc.gen)" \
+"$(sed '1d;s/\t//g;2s/.*/\.SS CONFIG VERSION MISMATCH/g;4s/.*/\.SS LIST VERSION MISMATCH/g' $DIR/doc/troubleshooting.doc)" > gen/doc/noaftodo.man
 
-gzip -c doc/noaftodo.man > doc/noaftodo.man.gz
+gzip -c gen/doc/noaftodo.man > gen/doc/noaftodo.man.gz
