771c6a457c3d6ea1ea53de3982c04510cb2bc456
GregTheMadMonk <yagreg7@gmail.com>
2021-01-31 22:26:44 +0400

    Give COMMAND mode commands descriptions

--- src/cui_modes/command.cpp
+++ src/cui_modes/command.cpp
@@ -16,26 +16,31 @@ void init() {
 	cvar_base_s::cvars["cmd.contexec"] = cvar_base_s::wrap_string(cui::contexec_regex_filter);
 
 	// command mode commands
+	// command "cmd.curs.left" - move command line curor to the left
 	cmd::cmds["cmd.curs.left"] = [] (const vector<string>& args) {
 		if (cui::command_cursor > 0) cui::command_cursor--;
 		return 0;
 	};
 
+	// command "cmd.curs.right" - move command line cursor to the right
 	cmd::cmds["cmd.curs.right"] = [] (const vector<string>& args) {
 		if (cui::command_cursor < cui::command.length()) cui::command_cursor++;
 		return 0;
 	};
 
+	// command "cmd.curs.home" - move command line cursor to the beginning of the line
 	cmd::cmds["cmd.curs.home"] = [] (const vector<string>& args) {
 		cui::command_cursor = 0;
 		return 0;
 	};
 
+	// command "cmd.curs.end" - move command line cursor to the end of the line
 	cmd::cmds["cmd.curs.end"] = [] (const vector<string>& args) {
 		cui::command_cursor = cui::command.length();
 		return 0;
 	};
 
+	// command "cmd.curs.word.home" - move command line cursor to the beginning of the word
 	cmd::cmds["cmd.curs.word.home"] = [] (const vector<string>& args) {
 		while (cui::command_cursor > 0) {
 			cui::command_cursor--;
@@ -46,6 +51,7 @@ void init() {
 		return 0;
 	};
 
+	// command "cmd.curs.word.end" - move command line cursor to the end of the word
 	cmd::cmds["cmd.curs.word.end"] = [] (const vector<string>& args) {
 		while (cui::command_cursor < cui::command.length()) {
 			cui::command_cursor++;
@@ -57,6 +63,7 @@ void init() {
 		return 0;
 	};
 
+	// command "cmd.history.up" - go one command up (earlier) in command history
 	cmd::cmds["cmd.history.up"] = [] (const vector<string>& args) {
 		if (cui::command_index > 0) {
 			if (cui::command_index == cui::command_history.size())
@@ -72,6 +79,7 @@ void init() {
 		return 0;
 	};
 
+	// command "cmd.history.down" - go one command down (later) in command history
 	cmd::cmds["cmd.history.down"] = [] (const vector<string>& args) {
 		if (cui::command_index < cui::command_history.size() - 1) {
 			cui::command_index++;
@@ -87,6 +95,7 @@ void init() {
 		return 0;
 	};
 
+	// command "cmd.backspace" - removes character before command line cursor
 	cmd::cmds["cmd.backspace"] = [] (const vector<string>& args) {
 		cui::command_index = cui::command_history.size();
 
@@ -100,6 +109,7 @@ void init() {
 		return 0;
 	};
 
+	// command "cmd.delete" - removes the character under command line cursor
 	cmd::cmds["cmd.delete"] = [] (const vector<string>& args) {
 		cui::command_index = cui::command_history.size();
 
@@ -110,6 +120,7 @@ void init() {
 		return 0;
 	};
 
+	// command "cmd.send" - sends input from command line for execution
 	cmd::cmds["cmd.send"] = [] (const vector<string>& args) {
 		if (cui::command != L"") {
 			exec(w_converter().to_bytes(cui::command));
@@ -121,6 +132,7 @@ void init() {
 		return 0;
 	};
 
+	// command "cmd.clear" - clears command line
 	cmd::cmds["cmd.clear"] = [] (const vector<string>& args) {
 		cui::command = L"";
 		cui::filter_history();
