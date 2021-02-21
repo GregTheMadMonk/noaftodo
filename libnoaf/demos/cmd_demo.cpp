d8809add40afdbb0b77c944ff122130370b0379a
GregTheMadMonk <yagreg7@gmail.com>
2021-02-21 23:21:20 +0300

    Separate demos

---
+++ libnoaf/demos/cmd_demo.cpp
@@ -0,0 +1,24 @@
+#include <csignal>
+#include <iostream>
+
+#include <cmd.hpp>
+#include <hooks.hpp>
+#include <log.hpp>
+
+using namespace std;
+using namespace noaf;
+
+int main(int argc, char* argv[]) {
+	setlocale(LC_ALL, "");
+
+	signal(SIGINT, noaf::exit);
+	signal(SIGTERM, noaf::exit);
+
+	log << lver(0);
+
+	string s;
+	while (getline(cin, s)) {
+		cmd::exec(s);
+	}
+	return 0;
+}
--- libnoaf/demos/ui_demo.cpp
+++ libnoaf/demos/ui_demo.cpp
@@ -1,7 +1,5 @@
 #include <csignal>
-#include <iostream>
 
-#include <cmd.hpp>
 #include <conarg.hpp>
 #include <hooks.hpp>
 #include <log.hpp>
@@ -15,49 +13,41 @@ int main(int argc, char* argv[]) {
 	setlocale(LC_ALL, "");
 
 	signal(SIGINT, noaf::exit);
+	signal(SIGTERM, noaf::exit);
 
-	log << lver(0);
+#ifdef __linux__
 
 	bool fb = false;
 
-	conarg::args()[{ "-h", "--help"}] = {
-		[] (const vector<string>& params) {
-			log << llev(2) << "Help message!!!" << lend;
-		},
-		0,
-		""
-	};
-#ifdef __linux__
 	conarg::args()[{ "-f" }] = {
 		[&] (const vector<string>& params) { fb = true; },
 		0,
 		""
 	};
-#endif
 
 	conarg::parse(argc, argv);
 
-#ifdef __linux__
 	if (fb) {
 		ui = make_shared<backend_framebuffer>();
 
 		ui_as<backend_framebuffer>()->dev_name = "fb0";
 		ui_as<backend_framebuffer>()->halfdelay_time = 3;
-	} else {
+	} else
 #endif
+	{
 		ui = make_shared<backend_ncurses>();
 
 		ui_as<backend_ncurses>()->charset = L"|-/\\\\/";
 		ui_as<backend_ncurses>()->halfdelay_time = 3;
-#ifdef __linux__
 	}
-#endif
 
 	ui->init();
 	ui->fill(true);
 
 	int frame = 0;
+
 	std::string input_name = "";
+
 	on_paint = [&] () {
 		frame++;
 		ui->clear();
@@ -88,11 +78,5 @@ int main(int argc, char* argv[]) {
 
 	ui->kill();
 
-	return 0;
-
-	string s;
-	while (getline(cin, s)) {
-		cmd::exec(s);
-	}
 	return 0;
 }
